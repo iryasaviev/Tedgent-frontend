84fb2fbeda2e336f267f1e278ee093cddf24a943
Deadcaton <ir.yasaviev@yandex.ru>
2020-01-30 12:11:24 +0300

    Work on the profile page

--- js/app.js
+++ js/app.js
@@ -2,16 +2,16 @@
 
 class LeftMenu {
     constructor() {
+        this.body = document.getElementById('body');
         this.menu = document.getElementById('leftMenu');
     }
 
     openOrClose(menu) {
-        this.menu.classList.toggle('left_menu-active');
+        this.body.classList.toggle('left_menu-active');
     }
 }
 
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
     let leftMenu = new LeftMenu();
-
     leftMenu.menu.getElementsByClassName('left_menu-hd--btn')[0].onclick = () => leftMenu.openOrClose();
 });
