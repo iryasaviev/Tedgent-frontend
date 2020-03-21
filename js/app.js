ce23b9a79ea0cb386bc5356b11a575c73da252a4
Deadcaton <ir.yasaviev@yandex.ru>
2020-03-21 22:21:50 +0300

    work on the design of sidebar & created a toggle button for night mode

--- js/app.js
+++ js/app.js
@@ -1,17 +1,23 @@
 'use strict'
 
-class LeftMenu {
+class Sidebar {
     constructor() {
         this.body = document.getElementById('body');
-        this.menu = document.getElementById('leftMenu');
+        this.menu = this.body.getElementsByClassName('sidebar')[0];
     }
 
-    openOrClose(menu) {
+    openOrClose() {
         this.body.classList.toggle('sidebar-active');
+    }
+
+    nightModeToggle() {
+        this.body.classList.toggle('night-mode');
     }
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    let leftMenu = new LeftMenu();
-    leftMenu.menu.getElementsByClassName('sidebar_hd--btn')[0].onclick = () => leftMenu.openOrClose();
+    let sidebar = new Sidebar();
+    sidebar.menu.getElementsByClassName('sidebar_hd--btn')[0].onclick = () => sidebar.openOrClose();
+    sidebar.menu.getElementsByClassName('nightMode')[0].onclick = () => sidebar.nightModeToggle();
+
 });
