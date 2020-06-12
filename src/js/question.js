46cfadbcf8bea9913fc41e2b2f25c6f12f77ca44
Deadcaton <ir.yasaviev@yandex.ru>
2020-06-12 19:21:38 +0300

    work on the more menu in the question add button

--- src/js/question.js
+++ src/js/question.js
@@ -100,70 +100,7 @@ export class Question {
     getQuestionItem(answerType, questionNum) {
         switch (answerType) {
             case '1':
-                return `<div class="test_create_bd-question js-test-question" data-question-num="${questionNum}" data-answers-type="1">
-                <div class="test_create_bd-question-col1">
-                  <div class="test_create_bd-question-col1-row">
-                    <button class="test_create_bd-question-col1-row--btn btn i-cross"></button>
-                    <input class="inp test_create_bd-question-col1-row--inp" name="qustion" type="text" placeholder="Вопрос №${questionNum}">
-                  </div>
-                  <div class="test_create_bd-question-col1-row test_create_bd-question-col1-row-image">
-                    <div class="test_create_bd-question-col1-image">
-                      <div class="test_create_bd-question-col1-image--bcg js-test-question-img-background"></div>
-                      <img class="test_create_bd-question-col1--img js-test-question-img">
-                    </div>
-                  </div>
-                  <div class="test_create_bd-question-col1-row test_create_bd-question-answers js-test-question-answers">
-                    <div class="test_create_bd-question-answer js-test-question-answer" data-answer-num="1">
-                      <div class="test-question-answer-checkbox js-test-question-answer-choice-btn">
-                        <input class="test-question-answer-checkbox--inp js-test-question-answer-choice-inp" id="answer${questionNum}1" name="answerCheck1"
-                          type="checkbox">
-                        <label class="js-test-question-answer-choice-label" for="answer${questionNum}1"></label>
-                      </div>
-                      <input class="inp test_create_bd-question-answer--inp js-test-question-answer-inp" name="answer" type="text" placeholder="Ответ №1">
-                      <button class="test_create_bd-question-answer--btn btn i-cross js-test-create-question-answer-delete-btn"></button>
-                    </div>
-                  </div>
-                  <div class="test_create_bd-question-col1-row test_create_bd-question-col1-row-add-answer">
-                    <button class="test_create_bd-question-answer-add--btn btn js-test-create-answer-add-btn">
-                      <span class="i-plus"></span>
-                      <span class="txt">Вариант</span>
-                    </button>
-                  </div>
-                </div>
-                <div class="test_create_bd-question-col2">
-                  <label class="test_create_bd-question-col2--btn btn i-image">
-                    <input class="test_create_bd-question-col2--inp js-test-create-question-image-inp" type="file"
-                      accept="image/*">
-                  </label>
-                  <div class="test_create_bd-question--select select js-test-create-answer-type-select">
-                    <button class="test_create_bd-question--select_hd select_hd">
-                      <div class="select_hd-value js-select-value" data-select-option-value="1">
-                        <div class="test_create_bd-question--select-option--icon i-checkbox"></div>
-                        <div class="test_create_bd-question--select-option--txt">Множественный выбор</div>
-                      </div>
-                      <div class="select_hd--icon i-down-arrow"></div>
-                    </button>
-                    <div class="select_bd">
-                      <button class="select_bd-option" data-select-option="1">
-                        <div class="test_create_bd-question--select-option--icon i-checkbox"></div>
-                        <div class="test_create_bd-question--select-option--txt">Множественный выбор</div>
-                      </button>
-                      <button class="select_bd-option" data-select-option="2">
-                        <div class="test_create_bd-question--select-option--icon i-radiobox"></div>
-                        <div class="test_create_bd-question--select-option--txt">Одиночный выбор</div>
-                      </button>
-                      <button class="select_bd-option" data-select-option="3">
-                        <div class="test_create_bd-question--select-option--icon i-text"></div>
-                        <div class="test_create_bd-question--select-option--txt">Текстовый ответ</div>
-                      </button>
-                      <button class="select_bd-option" data-select-option="4">
-                        <div class="test_create_bd-question--select-option--icon i-number"></div>
-                        <div class="test_create_bd-question--select-option--txt">Числовой ответ</div>
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </div>`;
+                return ``;
 
             case '2':
                 return ``;
