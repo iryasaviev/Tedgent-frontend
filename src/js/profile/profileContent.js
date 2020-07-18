4dd46b1ac48022e651046ae06d9459147417f0fa
Deadcaton <ir.yasaviev@yandex.ru>
2020-07-18 18:35:55 +0300

    work on the profile page

--- src/js/profile/profileContent.js
+++ src/js/profile/profileContent.js
@@ -20,7 +20,7 @@ export class ProfileContent {
             <div class="cnt-block_hd">
               <div class="profile-tests-title cnt-block_hd-title">
                 <span>Тесты</span>
-                <span class="profile-tests-title--counter">36</span>
+                <span class="profile-tests-title--counter">0</span>
               </div>
               <div class="profile-tests-menu">
                 <input class="inp profile-tests-menu--search" type="text" placeholder="Поиск">
@@ -40,146 +40,7 @@ export class ProfileContent {
               </div>
             </div>
             <div class="profile-tests_bd cnt-block_bd">
-              <div class="profile-test">
-                <div class="profile-test-logo test-subject-logo subject-bcg-1">м</div>
-                <div class="profile-test-info">
-                  <div class="profile-test-info--title">
-                    <a class="link" href="#">Тест по математике для 7 А</a>
-                  </div>
-                  <div class="raiting">
-                    <div class="raiting-stars">
-                      <div class="raiting-stars--scale"></div>
-                      <div class="raiting-star star-1"><span class="i-star2"></span></div>
-                      <div class="raiting-star star-2"><span class="i-star2"></span></div>
-                      <div class="raiting-star star-3"><span class="i-star2"></span></div>
-                      <div class="raiting-star star-4"><span class="i-star2"></span></div>
-                      <div class="raiting-star star-5"><span class="i-star2"></span></div>
-                    </div>
-                    <div class="raiting-star--voted">0</div>
-                  </div>
-                </div>
-                <div class="profile-test-date">16.07.2020</div>
-              </div>
-              <div class="profile-test">
-                <div class="profile-test-logo test-subject-logo subject-bcg-1">м</div>
-                <div class="profile-test-info">
-                  <div class="profile-test-info--title">
-                    <a class="link" href="#">Тест по математике для 7 А</a>
-                  </div>
-                  <div class="raiting">
-                    <div class="raiting-stars">
-                      <div class="raiting-stars--scale"></div>
-                      <div class="raiting-star star-1"><span class="i-star2"></span></div>
-                      <div class="raiting-star star-2"><span class="i-star2"></span></div>
-                      <div class="raiting-star star-3"><span class="i-star2"></span></div>
-                      <div class="raiting-star star-4"><span class="i-star2"></span></div>
-                      <div class="raiting-star star-5"><span class="i-star2"></span></div>
-                    </div>
-                    <div class="raiting-star--voted">0</div>
-                  </div>
-                </div>
-                <div class="profile-test-date">16.07.2020</div>
-              </div>
-              <div class="profile-test">
-                <div class="profile-test-logo test-subject-logo subject-bcg-1">м</div>
-                <div class="profile-test-info">
-                  <div class="profile-test-info--title">
-                    <a class="link" href="#">Тест по математике для 7 А</a>
-                  </div>
-                  <div class="raiting">
-                    <div class="raiting-stars">
-                      <div class="raiting-stars--scale"></div>
-                      <div class="raiting-star star-1"><span class="i-star2"></span></div>
-                      <div class="raiting-star star-2"><span class="i-star2"></span></div>
-                      <div class="raiting-star star-3"><span class="i-star2"></span></div>
-                      <div class="raiting-star star-4"><span class="i-star2"></span></div>
-                      <div class="raiting-star star-5"><span class="i-star2"></span></div>
-                    </div>
-                    <div class="raiting-star--voted">0</div>
-                  </div>
-                </div>
-                <div class="profile-test-date">16.07.2020</div>
-              </div>
-              <div class="profile-test">
-                <div class="profile-test-logo test-subject-logo subject-bcg-1">м</div>
-                <div class="profile-test-info">
-                  <div class="profile-test-info--title">
-                    <a class="link" href="#">Тест по математике для 7 А</a>
-                  </div>
-                  <div class="raiting">
-                    <div class="raiting-stars">
-                      <div class="raiting-stars--scale"></div>
-                      <div class="raiting-star star-1"><span class="i-star2"></span></div>
-                      <div class="raiting-star star-2"><span class="i-star2"></span></div>
-                      <div class="raiting-star star-3"><span class="i-star2"></span></div>
-                      <div class="raiting-star star-4"><span class="i-star2"></span></div>
-                      <div class="raiting-star star-5"><span class="i-star2"></span></div>
-                    </div>
-                    <div class="raiting-star--voted">0</div>
-                  </div>
-                </div>
-                <div class="profile-test-date">16.07.2020</div>
-              </div>
-              <div class="profile-test">
-                <div class="profile-test-logo test-subject-logo subject-bcg-1">м</div>
-                <div class="profile-test-info">
-                  <div class="profile-test-info--title">
-                    <a class="link" href="#">Тест по математике для 7 А</a>
-                  </div>
-                  <div class="raiting">
-                    <div class="raiting-stars">
-                      <div class="raiting-stars--scale"></div>
-                      <div class="raiting-star star-1"><span class="i-star2"></span></div>
-                      <div class="raiting-star star-2"><span class="i-star2"></span></div>
-                      <div class="raiting-star star-3"><span class="i-star2"></span></div>
-                      <div class="raiting-star star-4"><span class="i-star2"></span></div>
-                      <div class="raiting-star star-5"><span class="i-star2"></span></div>
-                    </div>
-                    <div class="raiting-star--voted">0</div>
-                  </div>
-                </div>
-                <div class="profile-test-date">16.07.2020</div>
-              </div>
-              <div class="profile-test">
-                <div class="profile-test-logo test-subject-logo subject-bcg-1">м</div>
-                <div class="profile-test-info">
-                  <div class="profile-test-info--title">
-                    <a class="link" href="#">Тест по математике для 7 А</a>
-                  </div>
-                  <div class="raiting">
-                    <div class="raiting-stars">
-                      <div class="raiting-stars--scale"></div>
-                      <div class="raiting-star star-1"><span class="i-star2"></span></div>
-                      <div class="raiting-star star-2"><span class="i-star2"></span></div>
-                      <div class="raiting-star star-3"><span class="i-star2"></span></div>
-                      <div class="raiting-star star-4"><span class="i-star2"></span></div>
-                      <div class="raiting-star star-5"><span class="i-star2"></span></div>
-                    </div>
-                    <div class="raiting-star--voted">0</div>
-                  </div>
-                </div>
-                <div class="profile-test-date">16.07.2020</div>
-              </div>
-              <div class="profile-test">
-                <div class="profile-test-logo test-subject-logo subject-bcg-1">м</div>
-                <div class="profile-test-info">
-                  <div class="profile-test-info--title">
-                    <a class="link" href="#">Тест по математике для 7 А</a>
-                  </div>
-                  <div class="raiting">
-                    <div class="raiting-stars">
-                      <div class="raiting-stars--scale"></div>
-                      <div class="raiting-star star-1"><span class="i-star2"></span></div>
-                      <div class="raiting-star star-2"><span class="i-star2"></span></div>
-                      <div class="raiting-star star-3"><span class="i-star2"></span></div>
-                      <div class="raiting-star star-4"><span class="i-star2"></span></div>
-                      <div class="raiting-star star-5"><span class="i-star2"></span></div>
-                    </div>
-                    <div class="raiting-star--voted">0</div>
-                  </div>
-                </div>
-                <div class="profile-test-date">16.07.2020</div>
-              </div>
+              <span>Пользователь не создал ещё ни одного теста.</span>
             </div>
           </div>
         </div>
